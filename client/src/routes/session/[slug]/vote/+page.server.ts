f4ed61e1f8102606dd0ab1e1fd6c5fd64321bad5
Nabil Amin <nabilamin1996@gmail.com>
2024-03-14 08:51:16 -0400

    implement basic front end voting logic

---
+++ client/src/routes/session/[slug]/vote/+page.server.ts
@@ -0,0 +1,30 @@
+export async function load() {
+	return {
+		restaurants: [
+            {
+                name: "Restaurant 1",
+                id: "1",
+                rating: 5,
+                review_count: 9
+            },
+            {
+                name: "Restaurant 2",
+                id: "2",
+                rating: 4,
+                review_count: 10
+            },
+            {
+                name: "Restaurant 3",
+                id: "3",
+                rating: 4.5,
+                review_count: 11
+            },
+            {
+                name: "Restaurant 4",
+                id: "4",
+                rating: 4.4,
+                review_count: 12
+            }
+        ]
+	};
+}
---
+++ client/src/routes/session/[slug]/vote/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    export let data;
+
+    export let dataLength = data.restaurants.length;
+
+    export let index = 0;
+
+    export let votingData = new Map<string, boolean>();
+
+    function handleClick(value: boolean) {
+        votingData.set(data.restaurants[index].id, value);
+
+        if (index + 1 == dataLength) {
+            console.log(votingData);
+        } else {
+            index++
+        }
+    }
+</script>
+
+<h1>{data.restaurants[index].name}</h1>
+<p>Rating: {data.restaurants[index].rating}</p>
+<p>Review Count: {data.restaurants[index].review_count}</p>
+<button on:click={() => handleClick(true)}>Yes</button>
+<button on:click={() => handleClick(false)}>No</button>
