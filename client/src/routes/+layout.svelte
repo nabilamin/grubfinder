0e5a83436de4be26ce3ea94cdc166093529ab7a7
Jon George <23463258+JonGeorge@users.noreply.github.com>
2024-03-17 17:17:30 -0400

    Add grubfinder background to layout

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+    import '../app.css';
+    import bgImg from "../../static/Grubfinder_background.svg";
+
+    const src = new URL('../../static/Grubfinder_background.svg', import.meta.url).href;
+</script>
+
+<style>
+    .bg {
+        width: 100%;
+    }
+</style>
+
+<div class="bg" style="background-image: url({src});
+                        width: 100%;
+                        height: 100%;">
+
+</div>
--- client/src/routes/+page.svelte
+++
@@ -1,2 +0,0 @@
-<h1>Hello World! We are GrubFinder</h1>
-<p>We are currently a work in progress. Check back soon!</p>
