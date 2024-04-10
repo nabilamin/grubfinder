e04cf079527a862fbd6eaa8c5f274f25998c4564
Jon George <23463258+JonGeorge@users.noreply.github.com>
2024-04-10 19:01:00 -0400

    Create Vote page

--- client/src/routes/session/[slug]/vote/+page.svelte
+++ client/src/routes/session/[slug]/vote/+page.svelte
@@ -1,8 +1,72 @@
 <script lang="ts">
-    import Vote from "../../../../components/Vote.svelte";
+    export let data: object;
 
-    export let data;
+    let index: number = 0;
+    let votingResponse = new Map<string, boolean>();
 
+    function handleClick(response: boolean) {
+        votingResponse.set(data.restaurants[index].id, response);
+
+        if (index + 1 == data.restaurants.length) {
+            // Send data to Lambda
+            console.log(votingResponse);
+        } else {
+            index++
+        }
+    }
 </script>
 
-<Vote data="{data}"></Vote>
+
+<h1>{data.restaurants[index].name}</h1>
+<p>Rating: {data.restaurants[index].rating}</p>
+<p>Review Count: {data.restaurants[index].review_count}</p>
+
+<div class="container">
+    <div class="row g-2 mb-4">
+        {#each data.restaurants[index].image_urls as {S}}
+            <div class="column">
+                <img src="{S}" alt="restaurant"/>
+            </div>
+        {/each}
+    </div>
+
+    <div class="row">
+        <div class="col-6">
+            <button on:click={() => handleClick(true)}>Yes</button>
+        </div>
+        <div class="col-6">
+            <button on:click={() => handleClick(false)}>No</button>
+        </div>
+    </div>
+</div>
+
+
+<style>
+    img {
+        /*margin: 0 auto;*/
+        width: 100%;
+    }
+
+
+    h1 {
+        font-family: 'Baloo 2 Variable', sans-serif;
+        color: #890000;
+        font-weight: 700;
+        margin-top: 12px;
+    }
+
+    .row {
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .column {
+        flex: 26%;
+        /*max-width: 26%;*/
+        padding: 0 4px;
+    }
+
+    button {
+        width: 100%;
+    }
+</style>
