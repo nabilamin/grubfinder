d249784a3d829abafcf4e3eef8c71a12bcbb2ea9
Jon George <23463258+JonGeorge@users.noreply.github.com>
2024-05-01 20:37:34 -0400

    Remove back button from loading page

--- client/src/components/Loading.svelte
+++ client/src/components/Loading.svelte
@@ -23,18 +23,6 @@
         100% { transform: rotate(360deg); }
     }
     
-    .pill-button {
-        display: inline-block;
-        background-color: #890000;
-        color: white;
-        padding: 10px 20px;
-        border-radius: 50px;
-        text-decoration: none;
-        font-family: 'Baloo 2 Variable', sans-serif;
-        font-size: 16px;
-        cursor: pointer;
-    }
-    
     </style>
     
     <script>
